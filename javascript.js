1a401d7cbafd145991e42c7bd31a26699ff2bf18
Henry <hpdunn2002@gmail.com>
2022-07-17 12:53:45 +0100

    js updates

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let a=10
+console.log(42+66);
+console.log(1+2+3+3+4+9);
+console.log((4+6+9)/77);
+console.log(a);
+console.log(9*a);
+let b=(4*a);
+console.log(b);
+let max=57;
+let actual=(max-13);
+let percentage=(actual/max);
+console.log(percentage);
