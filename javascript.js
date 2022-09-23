2a45d33751a7703c6d14c5282e1b46d070d6b8ee
Henry <hpdunn2002@gmail.com>
2022-09-23 14:00:36 +0100

    js learning update

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-console.log ("Hello, world!")
+// console.log ("Hello, world!")
+//
+//function favoriteAnimal(a) {
+//    console.log(a + " is my favorite animal!")
+//}
+//
+//favoriteAnimal('Goat')
+//
+//function showMessage() {
+//   alert( 'That was then, this is now bitch.' )
+//}
+//showMessage();
+
+let userName='Henry';
+function showMessage() {
+    userName= "Bob";
+    let message='Hello, '+userName;
+    alert(message)
+}
+showMessage();
+alert(userName);
