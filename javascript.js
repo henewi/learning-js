cadb039cf21c2fc6f3f339d75f87bc8cb3869f28
henewi <hpdunn2002@gmail.com>
2022-09-26 11:24:55 +0100

    fund pt2. strings

--- javascript.js
+++ javascript.js
@@ -1,37 +1,61 @@
-let a=10
-console.log(42+66);
-console.log(1+2+3+3+4+9);
-console.log((4+6+9)/77);
-console.log(a);
-console.log(9*a);
-let b=(4*a);
-console.log(b);
-let max=57;
-let actual=(max-13);
-let percentage=(actual/max);
-console.log(percentage);
+//let a=10
+//console.log(42+66);
+//console.log(1+2+3+3+4+9);
+//console.log((4+6+9)/77);
+//console.log(a);
+//console.log(9*a);
+//let b=(4*a);
+//console.log(b);
+//let max=57;
+//let actual=(max-13);
+//let percentage=(actual/max);
+//console.log(percentage);
 
 // New stuff after summer.
 
-let message;
-message='Alert Message!' // stores the string
-alert(message);
+//let message;
+//message='Alert Message!' // stores the string
+//alert(message);
 
-let message2='Hello';
-alert(message2); // combined into 2 lines from 3
+//let message2='Hello';
+//alert(message2); // combined into 2 lines from 3
 
-let message3;
-message3='start';
-message3='end'; // changes before the output
-alert(message3);
+//let message3;
+//message3='start';
+//message3='end'; // changes before the output
+//alert(message3);
 
-let resource='content';
-let copied;
-copied=resource; // copies resource's content to copied
-alert(copied);
+//let resource='content';
+//let copied;
+//copied=resource; // copies resource's content to copied
+//alert(copied);
 
 // test 
 
-const bday='22nd November 2002';
-alert(bday);
+//const bday='22nd November 2002';
+//alert(bday);
 
+// JS fundamentals 1 finished. 
+
+// FUNDAMENTALS 2.
+
+const string='testing the strings';
+//console.log(string);
+const badString=string;
+console.log(badString);
+
+const string2='It\'s in my DNA to do this.';
+console.log(string2); //it works!
+
+const nam='Henry';
+const greeting=`Hello, ${nam}`;
+console.log(greeting);
+
+const one='Hi';
+const two=' there!';
+const oneplustwo=`${one}${two}`;
+console.log(oneplustwo);
+
+let text="beans";
+let newtext=text.replace("beans", "carrots");
+console.log(newtext);
