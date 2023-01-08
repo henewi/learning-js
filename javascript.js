2cd77d4236d0ca1dad90748b51407844a7503f9d
henewi <hpdunn2002@gmail.com>
2023-01-08 12:45:10 +0000

    incrementing

--- javascript.js
+++ javascript.js
@@ -10,4 +10,15 @@ firstname = "henry";
 secondname = "dunn";
 age = "20";
 
+// Maths 
+
+var sum = 20+21;
+console.log(sum);
+console.log("use +, -, /, *.")
+
+// Incremrnting
+
+var myVar = 68;
+myVar++; 
+console.log(myVar);
 
